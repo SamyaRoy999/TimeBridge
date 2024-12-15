49bf9af83db07e94082a6d2645456e71805225d8
samya-roy <samyaroy999@gmail.com>
2024-12-15 20:00:54 +0600

    add Event List

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import "./App.css";
 import { CalendarDemo } from "./components/CalendarDemo";
 import { Card } from "@/components/ui/card";
+import EventList from "./components/EventList";
 
 function App() {
   const [allEvents, setAllEvents] = useState([]);
@@ -22,8 +23,9 @@ function App() {
         </Card>
       </div>
       {/* Right Section: Event List */}
-      <Card className="h-[90vh] col-span-2 m-8">
-        <h3 className="text-white font-bold mb-4">Event List</h3>
+      <Card className="h-[90vh] col-span-2 m-8 overflow-y-auto">
+        <h3 className="text-white font-bold my-4 text-center">Event List</h3>
+        <EventList allEvents={allEvents} />
       </Card>
     </div>
   );
--- src/components/EventList.jsx
+++ src/components/EventList.jsx
@@ -1,7 +1,27 @@
 import React from "react";
 
-const EventList = () => {
-  return <div></div>;
+const EventList = ({ allEvents }) => {
+  console.log(allEvents);
+
+  return (
+    <div>
+      <ul>
+        {allEvents.map((event, index) => (
+          <li
+            key={index}
+            className="text-white mb-2 border bg-[#18181B] p-3 m-2"
+          >
+            <p>({event.date})</p>
+            <h5>Event name: {event.name}</h5>
+            <p>{event.description}</p>
+            <p>
+              {event.startTime} to {event.endTime}
+            </p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default EventList;
